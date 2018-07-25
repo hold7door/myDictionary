35d30b04889e47b603458501bfb84684838f27d7
zahlen <arpitpathak97@gmail.com>
2018-07-25 13:09:43 +0530

    Create notify.py

---
+++ notify.py
@@ -0,0 +1,57 @@
+from win32api import *
+from win32gui import *
+import win32con
+import sys, os
+import struct
+import time
+import MySQLdb
+
+database = MySQLdb.connect(user = '' , passwd = '' , db = '')     
+db = database.cursor()
+
+class WindowsBalloonTip:
+    def __init__(self, title, msg):
+        message_map = {
+                win32con.WM_DESTROY: self.OnDestroy,
+        }
+        # Register the Window class.
+        wc = WNDCLASS()
+        hinst = wc.hInstance = GetModuleHandle(None)
+        wc.lpszClassName = "PythonTaskbar"
+        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        classAtom = RegisterClass(wc)
+        # Create the Window.
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = CreateWindow( classAtom, "Taskbar", style, \
+                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+                0, 0, hinst, None)
+        UpdateWindow(self.hwnd)
+        iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+           hicon = LoadImage(hinst, iconPathName, \
+                    win32con.IMAGE_ICON, 0, 0, icon_flags)
+        except:
+          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, \
+                         (self.hwnd, 0 , NIF_INFO, win32con.WM_USER+20,\
+                          hicon, "Balloon  tooltip",msg,100,title,NIIF_INFO))
+        # self.show_balloon(title, msg)
+        time.sleep(10)
+        DestroyWindow(self.hwnd)
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (self.hwnd, 0)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        PostQuitMessage(0) # Terminate the app.
+
+def balloon_tip(title, msg):
+	w=WindowsBalloonTip(title, msg)
+	
+if __name__ == '__main__':
+	db.execute("""select * from words order by (rand()+rand())%(select count(*) from words) limit 1""")
+	res = db.fetchall()
+	print (res)
+	balloon_tip(res[0][0] + ' ' + res[0][3] , res[0][1])
